629c1ec4b59ba9041c0dca631730d162724cb9bf
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-09-18 22:20:08 +0200

    joined both request to get the weather of a city by putting its lattitude and longitude

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-const chalk = require('chalk')
-const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/de5697dbd2d6096915de5183a8374d3f/37.8267,-122.4233?units=si&lang=es'
+const chalk = require('chalk')  //This is to color console.log messages
+const request = require('request') //This is to make http requests
+const fs = require('fs')
 
-request({url: url, json: true }, (error, response)=>{
-  // console.log(response.body.currently)
+const city_int = 'Chicago'
 
-  console.log(response.body.daily.data[0].summary + " Right now in " + chalk.green(response.body.timezone) + " is currently " + chalk.green(response.body.currently.temperature) + " celsius degrees out. There is a " + chalk.green(response.body.currently.precipProbability) + "% chance of rain")
+const url_mapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + city_int + '.json?access_token=pk.eyJ1IjoiamFpbWVqYWNvYm8iLCJhIjoiY2swb2JoMmt6MDZkbzNsbjZldDNudTZicSJ9.d_UI7RuoLl4vQJao9IiEAw&limit=1'
 
 
+// Requesting the "Mapbox" API
+request({url: url_mapbox, json: true}, (error, response)=>{
+  if(!error){
+    let longitude = response.body.features[0].center[0]
+    let lattitude = response.body.features[0].center[1]
+
+    
+    // Requesting the "Darksky" API
+    const url_darksky = 'https://api.darksky.net/forecast/de5697dbd2d6096915de5183a8374d3f/' + lattitude + ',' + longitude + '?units=si&lang=es'
+
+    request({url: url_darksky, json: true }, (error, response)=>{
+      if(!error){
+        console.log(response.body.daily.data[0].summary + " Right now in " + chalk.green(response.body.timezone) + " is currently " + chalk.green(response.body.currently.temperature) + " celsius degrees out. There is a " + chalk.green(response.body.currently.precipProbability) + "% chance of rain")
+      } else {
+        console.log(chalk.red.inverse('There was an error calling the "Darksky" API'))
+      }
+    })
+  } else {
+    console.log(chalk.red.inverse('There was an error calling the "Mapbox API'))
+  }
 })
+
+
+
+
+
